2035ae50043982cc56371fb035fec67deac85d68
Masaru Koizumi <masaru@mk-mode.com>
2016-07-27 14:09:25 +0900

    UPD: Changed details of methods.

--- lib/mk_time/calc.rb
+++ lib/mk_time/calc.rb
@@ -1,24 +1,193 @@
 module MkTime
   class Calc
-    attr_reader :utc, :jst, :jd, :t, :utc_tai, :leap_sec, :dut1,
-                :ut1, :tai, :dt, :tt, :tcg, :tcb, :tdb
     include Compute
-    alias leap_sec utc_tai
+    attr_reader :utc
 
+    #=========================================================================
+    # Initialize
+    #
+    # @param:  arg   (引数)
+    # @return: <none>
+    #=========================================================================
     def initialize(arg)
-      @utc      = arg
-      @jst      = utc2jst(@utc)
-      @jd       = gc2jd(@utc)
-      @t        = jd2t(@jd)
-      @utc_tai  = get_utc_tai(@utc)
-      @dut1     = get_dut1(@utc)
-      @ut1      = utc2ut1(@utc)
-      @tai      = utc2tai(@utc)
-      @dt       = calc_dt(@utc)
-      @tt       = tai2tt(@tai)
-      @tcg      = tt2tcg(@tt)
-      @tcb      = tt2tcb(@tt)
-      @tdb      = tcb2tdb(@tcb)
+      @utc = arg
+    end
+
+    #=========================================================================
+    # JST (日本標準時)
+    #
+    # @param:  <none>
+    # @return: @js  (Time Object)
+    #=========================================================================
+    def jst
+      @jst = utc2jst(@utc)
+    rescue => e
+      raise
+    end
+
+    #=========================================================================
+    # JD (ユリウス日)
+    #
+    # @param:  <none>
+    # @return: @jd  (days)
+    #=========================================================================
+    def jd
+      @jd = gc2jd(@utc)
+    rescue => e
+      raise
+    end
+
+    #=========================================================================
+    # T (ユリウス世紀数)
+    #
+    # @param:  <none>
+    # @return: @t  (centurys)
+    #=========================================================================
+    def t
+      @jd ||= gc2jd(@utc)
+      @t = jd2t(@jd)
+    rescue => e
+      raise
+    end
+
+    #=========================================================================
+    # UTC - TAI (協定世界時と国際原子時の差 = うるう秒の総和)
+    #
+    # @param:  <none>
+    # @return: @utc_tai  (seconds)
+    #=========================================================================
+    def utc_tai
+      @utc_tai = get_utc_tai(@utc)
+    rescue => e
+      raise
+    end
+
+    #=========================================================================
+    # LEAP SECONDS (= 協定世界時と国際原子時の差)
+    #
+    # @param:  <none>
+    # @return: @leap_sec  (seconds)
+    #=========================================================================
+    def leap_sec
+      @utc_tai ||= get_utc_tai(@utc)
+      @leap_sec = @utc_tai
+    rescue => e
+      raise
+    end
+
+    #=========================================================================
+    # DUT1 (UT1(世界時1) と UTC(協定世界時)の差)
+    #
+    # @param:  <none>
+    # @return: @dut1  (seconds)
+    #=========================================================================
+    def dut1
+      @dut1 = get_dut1(@utc)
+    rescue => e
+      raise
+    end
+
+    #=========================================================================
+    # deltaT (TT(地球時) と UT1(世界時1)の差)
+    #
+    # @param:  <none>
+    # @return: @dt  (seconds)
+    #=========================================================================
+    def dt
+      @utc_tai ||= get_utc_tai(@utc)
+      @dut1    ||= get_dut1(@utc)
+      @dt = calc_dt(@utc)
+    rescue => e
+      raise
+    end
+
+    #=========================================================================
+    # TAI (国際原子時)
+    #
+    # @param:  <none>
+    # @return: @tai  (Time Object)
+    #=========================================================================
+    def tai
+      @utc_tai ||= get_utc_tai(@utc)
+      @tai = utc2tai(@utc)
+    rescue => e
+      raise
+    end
+
+    #=========================================================================
+    # UT1 (世界時1)
+    #
+    # @param:  <none>
+    # @return: @ut1  (Time Object)
+    #=========================================================================
+    def ut1
+      @dut1 ||= get_dut1(@utc)
+      @ut1 = utc2ut1(@utc)
+    rescue => e
+      raise
+    end
+
+    #=========================================================================
+    # TT (地球時)
+    #
+    # @param:  <none>
+    # @return: @tt  (Time Object)
+    #=========================================================================
+    def tt
+      @utc_tai ||= get_utc_tai(@utc)
+      @tai     ||= utc2tai(@utc)
+      @tt = tai2tt(@tai)
+    rescue => e
+      raise
+    end
+
+    #=========================================================================
+    # TCG (地球重心座標時)
+    #
+    # @param:  <none>
+    # @return: @tcg  (Time Object)
+    #=========================================================================
+    def tcg
+      @utc_tai ||= get_utc_tai(@utc)
+      @tai     ||= utc2tai(@utc)
+      @tt      ||= tai2tt(@tai)
+      @jd      ||= gc2jd(@utc)
+      @tcg = tt2tcg(@tt)
+    rescue => e
+      raise
+    end
+
+    #=========================================================================
+    # TCB (太陽系重心座標時)
+    #
+    # @param:  <none>
+    # @return: @tcb  (Time Object)
+    #=========================================================================
+    def tcb
+      @utc_tai ||= get_utc_tai(@utc)
+      @tai     ||= utc2tai(@utc)
+      @tt      ||= tai2tt(@tai)
+      @jd      ||= gc2jd(@utc)
+      @tcb = tt2tcb(@tt)
+    rescue => e
+      raise
+    end
+
+    #=========================================================================
+    # TDB (太陽系力学時)
+    #
+    # @param:  <none>
+    # @return: @tdb  (Time Object)
+    #=========================================================================
+    def tdb
+      @utc_tai ||= get_utc_tai(@utc)
+      @tai     ||= utc2tai(@utc)
+      @tt      ||= tai2tt(@tai)
+      @jd      ||= gc2jd(@utc)
+      @tcb     ||= tt2tcb(@tt)
+      @tdb = tcb2tdb(@tcb)
+    rescue => e
+      raise
     end
   end
 end
--- spec/mk_time/calc_spec.rb
+++ spec/mk_time/calc_spec.rb
@@ -1,87 +1,138 @@
 require "spec_helper"
 
 describe MkTime::Calc do
-  let(:c) { MkTime::Calc.new(Time.new(2016, 7, 26, 0, 0, 0, "+00:00")) }
+  let(:c) { MkTime::Calc.new(Time.new(2016, 7, 27, 0, 0, 0, "+00:00")) }
 
-  context "#new(2016-07-26 00:00:00 +00:00)" do
+  context "#new(2016-07-27 00:00:00 +00:00)" do
     context "object" do
       it { expect(c).to be_an_instance_of(MkTime::Calc) }
     end
 
     context "@utc" do
-      it { expect(c.instance_variable_get(:@utc).strftime("%Y-%m-%d %H:%M:%S")).to eq "2016-07-26 00:00:00" }
+      it { expect(c.instance_variable_get(:@utc).strftime("%Y-%m-%d %H:%M:%S")).to eq "2016-07-27 00:00:00" }
     end
 
-    context "@jst" do
-      it { expect(c.instance_variable_get(:@jst).strftime("%Y-%m-%d %H:%M:%S")).to eq "2016-07-26 09:00:00" }
-    end
+    context "#methods" do
+      let(:utc) { Time.new(2016, 7, 27, 0, 0, 0, "+00:00") }
 
-    context "@jd" do
-      it { expect(c.instance_variable_get(:@jd)).to eq 2457595.5 }
-    end
+      context ".jst" do
+        subject do
+          c.instance_variable_set(:@utc, utc)
+          c.jst
+        end
+        it do
+          expect(subject).to                        eq Time.new(2016, 7, 27, 9, 0, 0, "+09:00")
+          expect(c.instance_variable_get(:@jst)).to eq Time.new(2016, 7, 27, 9, 0, 0, "+09:00")
+        end
+      end
 
-    context "@t" do
-      it { expect(c.instance_variable_get(:@t)).to be_within(1.0e-16).of(0.1656536618754278) }
-    end
+      context ".jd" do
+        subject { c.jd }
+        it do
+          expect(subject).to                       eq 2457596.5
+          expect(c.instance_variable_get(:@jd)).to eq 2457596.5
+        end
+      end
 
-    context "@utc_tai" do
-      it { expect(c.instance_variable_get(:@utc_tai)).to eq -36 }
-    end
+      context ".t" do
+        subject { c.t }
+        it do
+          expect(subject).to                      be_within(1.0e-17).of(0.16568104038329912)
+          expect(c.instance_variable_get(:@t)).to be_within(1.0e-17).of(0.16568104038329912)
+        end
+      end
 
-    context "@dut1" do
-      it { expect(c.instance_variable_get(:@dut1)).to eq -0.2 }
-    end
+      context ".utc_tai" do
+        subject { c.utc_tai }
+        it do
+          expect(subject).to                            eq -36
+          expect(c.instance_variable_get(:@utc_tai)).to eq -36
+        end
+      end
 
-    context "@ut1" do
-      it { expect(c.instance_variable_get(:@ut1).strftime("%Y-%m-%d %H:%M:%S")).to eq "2016-07-25 23:59:59" }
-    end
+      context ".leap_sec" do
+        subject { c.leap_sec }
+        it do
+          expect(subject).to                             eq -36
+          expect(c.instance_variable_get(:@leap_sec)).to eq -36
+        end
+      end
 
-    context "@ut1 (usec)" do
-      it { expect((c.instance_variable_get(:@ut1).usec / 1000.0).round).to eq 800 }
-    end
+      context ".dut1" do
+        subject { c.dut1 }
+        it do
+          expect(subject).to                         eq -0.2
+          expect(c.instance_variable_get(:@dut1)).to eq -0.2
+        end
+      end
 
-    context "@tai" do
-      it { expect(c.instance_variable_get(:@tai).strftime("%Y-%m-%d %H:%M:%S")).to eq "2016-07-26 00:00:36" }
-    end
+      context ".ut1" do
+        subject { c.ut1 }
+        it do
+          expect(subject.strftime("%Y-%m-%d %H:%M:%S")).to                        eq "2016-07-26 23:59:59"
+          expect((subject.usec / 1000.0).round).to                                eq 800
+          expect(c.instance_variable_get(:@ut1).strftime("%Y-%m-%d %H:%M:%S")).to eq "2016-07-26 23:59:59"
+          expect((c.instance_variable_get(:@ut1).usec / 1000.0).round).to         eq 800
+        end
+      end
 
-    context "@tai (usec)" do
-      it { expect((c.instance_variable_get(:@tai).usec / 1000.0).round).to eq 0 }
-    end
+      context ".tai" do
+        subject { c.tai }
+        it do
+          expect(subject.strftime("%Y-%m-%d %H:%M:%S")).to                        eq "2016-07-27 00:00:36"
+          expect((subject.usec / 1000.0).round).to                                eq 0
+          expect(c.instance_variable_get(:@tai).strftime("%Y-%m-%d %H:%M:%S")).to eq "2016-07-27 00:00:36"
+          expect((c.instance_variable_get(:@tai).usec / 1000.0).round).to         eq 0
+        end
+      end
 
-    context "@dt" do
-      it { expect(c.instance_variable_get(:@dt)).to eq 68.384 }
-    end
+      context ".dt" do
+        subject { c.dt }
+        it do
+          expect(subject).to                       eq 68.384
+          expect(c.instance_variable_get(:@dt)).to eq 68.384
+        end
+      end
 
-    context "@tt" do
-      it { expect(c.instance_variable_get(:@tt).strftime("%Y-%m-%d %H:%M:%S")).to eq "2016-07-26 00:01:08" }
-    end
+      context ".tt" do
+        subject { c.tt }
+        it do
+          expect(subject.strftime("%Y-%m-%d %H:%M:%S")).to                       eq "2016-07-27 00:01:08"
+          expect((subject.usec / 1000.0).round).to                               eq 184
+          expect(c.instance_variable_get(:@tt).strftime("%Y-%m-%d %H:%M:%S")).to eq "2016-07-27 00:01:08"
+          expect((c.instance_variable_get(:@tt).usec / 1000.0).round).to         eq 184
+        end
+      end
 
-    context "@tt (usec)" do
-      it { expect((c.instance_variable_get(:@tt).usec / 1000.0).round).to eq 184 }
-    end
+      context ".tcg" do
+        subject { c.tcg }
+        it do
+          expect(subject.strftime("%Y-%m-%d %H:%M:%S")).to                        eq "2016-07-27 00:01:09"
+          expect((subject.usec / 1000.0).round).to                                eq 54
+          expect(c.instance_variable_get(:@tcg).strftime("%Y-%m-%d %H:%M:%S")).to eq "2016-07-27 00:01:09"
+          expect((c.instance_variable_get(:@tcg).usec / 1000.0).round).to         eq 54
+        end
+      end
 
-    context "@tcg" do
-      it { expect(c.instance_variable_get(:@tcg).strftime("%Y-%m-%d %H:%M:%S")).to eq "2016-07-26 00:01:09" }
-    end
+      context "@tcb" do
+        subject { c.tcb }
+        it do
+          expect(subject.strftime("%Y-%m-%d %H:%M:%S")).to                        eq "2016-07-27 00:01:27"
+          expect((subject.usec / 1000.0).round).to                                eq 545
+          expect(c.instance_variable_get(:@tcb).strftime("%Y-%m-%d %H:%M:%S")).to eq "2016-07-27 00:01:27"
+          expect((c.instance_variable_get(:@tcb).usec / 1000.0).round).to         eq 545
+        end
+      end
 
-    context "@tcg (usec)" do
-      it { expect((c.instance_variable_get(:@tcg).usec / 1000.0).round).to eq 54 }
-    end
-
-    context "@tcb" do
-      it { expect(c.instance_variable_get(:@tcb).strftime("%Y-%m-%d %H:%M:%S")).to eq "2016-07-26 00:01:27" }
-    end
-
-    context "@tcb (usec)" do
-      it { expect((c.instance_variable_get(:@tcb).usec / 1000.0).round).to eq 543 }
-    end
-
-    context "@tdb" do
-      it { expect(c.instance_variable_get(:@tdb).strftime("%Y-%m-%d %H:%M:%S")).to eq "2016-07-26 00:01:08" }
-    end
-
-    context "@tdb (usec)" do
-      it { expect((c.instance_variable_get(:@tdb).usec / 1000.0).round).to eq 184 }
+      context "@tdb" do
+        subject { c.tdb }
+        it do
+          expect(subject.strftime("%Y-%m-%d %H:%M:%S")).to                        eq "2016-07-27 00:01:08"
+          expect((subject.usec / 1000.0).round).to                                eq 184
+          expect(c.instance_variable_get(:@tdb).strftime("%Y-%m-%d %H:%M:%S")).to eq "2016-07-27 00:01:08"
+          expect((c.instance_variable_get(:@tdb).usec / 1000.0).round).to         eq 184
+        end
+      end
     end
   end
 end
